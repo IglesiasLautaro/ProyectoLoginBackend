b1cf4ce7ab791f7d63f7c47e80def44f133e8768
lucaputo <lauiglesias2004@gmail.com>
2024-01-23 18:51:53 -0300

    Tercera Pre Entrega del PF

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -1,10 +1,10 @@
-import * as cartDao from "../persistence/carts.dao.js"
+// import * as cartDao from "../persistence/carts.dao.js"
 
-const getCart=(user) =>{
-    return cartDao.addCart(user);
-}
-const createCart =(name)=>{
-    return  cartDao.addCart(name);
-}
+// const getCart=(user) =>{
+//     return cartDao.addCart(user);
+// }
+// const createCart =(name)=>{
+//     return  cartDao.addCart(name);
+// }
 
-export {createCart,getCart}
+// export {createCart,getCart}
